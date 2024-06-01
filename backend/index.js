45dd2069e3dfcaaa9b5782027cc720c70f737c81
Masco2t <ma924145@ucf.edu>
2024-06-01 14:40:27 -0400

    version 1.4

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ dotenv.config();
 var db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE
 });
 
